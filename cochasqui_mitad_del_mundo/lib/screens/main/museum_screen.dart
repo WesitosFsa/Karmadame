73170574b0c6b7850f8884ed324e8c3bc5b134af
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2025-05-13 21:55:52 -0500

    realidad aumentada con arcore archivos .gltf y plugin nuevo problema con dispositivos no compatibles con servicios de google arcore

--- cochasqui_mitad_del_mundo/lib/screens/main/museum_screen.dart
+++ cochasqui_mitad_del_mundo/lib/screens/main/museum_screen.dart
@@ -1,11 +1,11 @@
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
+import 'package:ar_flutter_plugin_updated/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin_updated/managers/ar_anchor_manager.dart';
+import 'package:ar_flutter_plugin_updated/managers/ar_location_manager.dart';
+import 'package:ar_flutter_plugin_updated/managers/ar_object_manager.dart';
+import 'package:ar_flutter_plugin_updated/managers/ar_session_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
-import 'package:ar_flutter_plugin/datatypes/node_types.dart';
-import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:ar_flutter_plugin_updated/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin_updated/models/ar_node.dart';
 import 'package:vector_math/vector_math_64.dart';
 
 class MuseumScreen extends StatefulWidget {
@@ -46,17 +46,15 @@ class _MuseumScreenState extends State<MuseumScreen> {
     arSessionManager.onInitialize(
       showFeaturePoints: false,
       showPlanes: true,
-      customPlaneTexturePath: null,
       showWorldOrigin: false,
     );
-
     arObjectManager.onInitialize();
   }
 
   Future<void> addModel() async {
     var newNode = ARNode(
-      type: NodeType.localGLTF2, // <- CORREGIDO
-      uri: "assets/modelo.glb",  // <- asegúrate que esté en pubspec.yaml
+      type: NodeType.localGLTF2, 
+      uri: "assets/modelos/Chicken_01.gltf",
       scale: Vector3(0.2, 0.2, 0.2),
       position: Vector3(0.0, 0.0, -1.0),
       rotation: Vector4(1.0, 0.0, 0.0, 0.0),
